f4d7f50b747ce64b22318b9dd123f06d4d18c6bc
filiptqvist <filip_t@hotmail.se>
2016-11-28 18:15:44 +0100

    Update dragdrop.js

--- dragdrop.js
+++ dragdrop.js
@@ -1,13 +1,28 @@
-function allowDrop(ev) {
-    ev.preventDefault();
+function allowDrop(allowdropevent) {
+    allowdropevent.target.style.color = 'black';
+    allowdropevent.preventDefault();
 }
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+function drag(dragevent) {
+    dragevent.dataTransfer.setData("text", dragevent.target.id);
+    dragevent.target.style.color = 'black';
 }
 
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+function drop(dropevent) {
+	
+  dropevent.preventDefault();
+  var data=dropevent.dataTransfer.getData("text");
+  var nodeCopy = document.getElementById(data).cloneNode(true);
+  nodeCopy.id = "drink_in_cart";
+  dropevent.target.appendChild(nodeCopy);
+}
+
+function ClearCart() {
+
+	ClearDrink('drink_in_cart');
+}
+
+function ClearDrink(elementID) {
+
+    document.getElementById(elementID).innerHTML = "";
 }
