f85d70626d02468640a0ec49c0d0109d1bb21714
filiptqvist <filip_t@hotmail.se>
2016-11-28 18:22:57 +0100

    Add files via upload

---
+++ api.js
@@ -0,0 +1,44 @@
+﻿function APIConnect() {
+    var baseURL = 'http://pub.jamaica-inn.net/fpdb/api.php',
+		username = '',
+		password = '';
+	
+	function constructURL(params) { 
+        var urlLink = baseURL + '?' + 'username=' + username + '&' + 'password=' + password;
+        
+        for (var key in params) {
+            urlLink = urlLink + '&' + key + '=' + params[key];
+        }
+        
+        return urlLink;
+    }
+	
+	function request(url, callback) {
+        var xhr = new XMLHttpRequest();
+        /*url = 'http://www.someurl.com?param1=value1&param2=value2';*/
+	
+        xhr.open('GET', url);
+        xhr.onreadystatechange = function() {
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                callback(this.responseText)
+                console.log(this.responseText);
+            }
+        };
+        xhr.send();
+    }
+	
+	this.setUser = function(un, pw) {
+		username = un;
+		password = pw;
+	};
+	
+	this.fetchUsers = function(callback) {
+		var url = constructURL({action: 'user_get_all'});
+		request(url, callback);
+	};
+	
+	this.fetchIOU = function(callback) {
+        var url = constructURL({action: 'iou_get'});
+        request(url, callback);
+    };
+}
---
+++ script.js
@@ -0,0 +1,32 @@
+﻿function docLoaded(fn) {
+	if (document.readyState !== 'loading') {
+        fn();
+	} else {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
+}
+
+function connectToAPI() {
+    var api = new APIConnect();
+	/*btn = document.getElementById("b1");*/
+
+    api.setUser('jorass', 'jorass');
+    /*btn.addEventListener('click', function() { loadUsers(api) });*/
+
+    api.fetchIOU(function(usr) {  
+        
+        var json = JSON.parse(usr);
+        var payload = json.payload;
+        
+        var name = payload[0].first_name;
+        var credit = payload[0].assets;
+        
+        document.querySelector('.username').innerHTML = name;
+        document.querySelector('.credit').innerHTML = credit;
+        
+    });
+}
+
+function pageLoaded() {
+	connectToAPI();
+}
