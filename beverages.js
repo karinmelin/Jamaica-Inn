5599ae3509a1939b097f4e7d8d49b898f12571f9
karinmelin <kristinamelin@hotmail.com>
2016-11-30 16:40:51 +0100

    Add beverages.js

---
+++ beverages.js
@@ -0,0 +1,90 @@
+// check if page loaded
+function docLoaded(fn) {
+    if (document.readyState !== 'loading') {
+        fn();
+	} else {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
+}
+
+// connects to the server
+function connectAPI() {
+    var api = new APIConnect();
+    
+    api.setUser('jorass', 'jorass');
+    
+    api.fetchIOU(function(usr) {
+        
+        var json = JSON.parse(usr);
+        var payload = json.payload;
+        
+        var assets = payload[0].assets
+        var name = payload[0].first_name;
+        document.querySelector('#assets').innerHTML = assets;
+        document.querySelector('#headerName').innerHTML = name;
+    });
+    
+    api.fetchBev(function(bevList) {
+        
+        var json = JSON.parse(bevList);
+        var payload = json.payload;
+        
+        for (var i = 1; i < payload.length; i++) {
+            var beer_name = payload[39+i].namn;
+            var beer_td = 'drink' + i;
+            document.querySelector('#' + beer_td).innerHTML = beer_name;
+            
+            var beer_id = payload[i].beer_id;
+            
+            api.fetchBevType(function(bev, beer_id){
+            
+                var json = JSON.parse(bev);
+                var payload_type = json.payload;
+                
+                var beer_type = payload_type[0].alkoholhalt;
+                document.querySelector('#drink1type').innerHTML = beer_type;
+                
+            });
+            
+        }
+        
+    });
+    
+    /*api.fetchBevType(function(beverage) {
+        
+        var json = JSON.parse(beverage);
+        var payload = json.payload;
+        
+        for (var i = 1; i < payload.length; i++) {
+            var beer_name = payload[39+i].namn;
+            var beer_id = 'drink' + i;
+            document.querySelector('#' + beer_id).innerHTML = beer_name;
+        }
+        
+    });*/
+    
+    
+    api.fetchPrevDrinks(function(list) {
+        
+        var json = JSON.parse(list);
+        var payload = json.payload;
+        
+        for (var i = 0; i < payload.length; i++) {
+            
+            var drink = payload[i+2].namn;
+            
+            var para = document.createElement("p");
+            var node = document.createTextNode(drink);
+            para.appendChild(node);
+
+            var element = document.getElementById("prevDrinks");
+            element.appendChild(para);
+        }
+        
+    });
+    
+}
+
+function bevPageLoaded() {
+	connectAPI();
+}
