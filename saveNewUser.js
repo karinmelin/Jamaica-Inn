698b49eabd08cada5f6237f6fb61b7674e107b03
filiptqvist <filip_t@hotmail.se>
2016-12-09 11:19:22 +0100

    comments and stuff

--- saveNewUser.js
+++ saveNewUser.js
@@ -1,3 +1,8 @@
+/* The purpose of this file is that the admin should be able to create a new user or edit an existing user
+File was created by by Filip Törnqvist  */
+
+/*This function is called by clicking the button saveuser_button that exits in the file admin_manage_users.html
+The function catches what the admin enters into the fields on the page admin_manage_users.html and passes it to the function setNewUser()*/
 function saveNewUser() {
 	var new_username;
 	var new_password;
@@ -16,7 +21,9 @@ function saveNewUser() {
 	setNewUser(new_username, new_password, first_name,last_name, email, phone);
 }
 
-
+/*This function accepts arguments from the function above and sets the entered username and password as localStorage variables.
+The main part of the function is that it passes the arguments from the previous function to the function api.setNewuser 
+(located in the file api.js), which in turn updates an existing user or creates a new one. */
 function setNewUser(new_username, new_password, first_name,last_name, email, phone) {
 	var api = new APIConnect();
     
