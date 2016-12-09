de160c9724feeddd89504f839814d25876b2be3b
karinmelin <kristinamelin@hotmail.com>
2016-12-09 16:35:58 +0100

    translation admin_view_beverages

--- view_all_beverages.js
+++ view_all_beverages.js
@@ -14,6 +14,7 @@ function docLoaded(fn) {
 It also stores the username and password in localStorage. The API function used here is fetchBev, which is located
 in api.js */
 function loadAllBeverages() {
+    checkLang();
 	var api = new APIConnect();
     
     var username = localStorage.localUsername;
@@ -25,7 +26,7 @@ function loadAllBeverages() {
 		var json = JSON.parse(list);
         var payload = json.payload;
 
-        for (var i = 0; i < payload.length; i++) {
+        for (var i = 0; i < payload.length-7; i++) {
 			var namn = payload[i+7].namn;
             var namn2 = payload[i+7].namn2;
             var sbl_price = payload[i+7].sbl_price;
@@ -38,7 +39,7 @@ function loadAllBeverages() {
 			var node = document.createTextNode(namn + ' ' + namn2 + '\u00A0\u00A0\u00A0\u00A0' + price + ':-' +'\u00A0\u00A0\u00A0\u00A0' + count);
 			para.appendChild(node);
 
-			var userList = document.getElementById("all_beverages");
+			var userList = document.getElementById("view_beverages");
 			userList.appendChild(para);
 			
 
