3b80f5bbf41eb265c9117dd9b8d7635f16d9f51a
SiFalorSum <nilshansander@hotmail.com>
2017-01-02 13:22:20 +0100

    plus/minus buttons in cart

--- dragdrop.js
+++ dragdrop.js
@@ -58,29 +58,58 @@ function addToCart(cart, drink_id, drink_ptag) {
             var namenode = document.createTextNode(drink_name);
             var amountnode = document.createTextNode(amount);
             var pricenode = document.createTextNode(price);
-            var spacenode = document.createTextNode(" ")
-            var priceendnode = document.createTextNode(":-")
-            var para1 = document.createElement("p");
-            var para2 = document.createElement("p");
-            var para3 = document.createElement("p");
-            var para4 = document.createElement("p");
-            var para5 = document.createElement("br");
-            var para6 = document.createElement("p");
-            para1.setAttribute('class', 'drink_cart');
-            para2.setAttribute('class', 'amount_cart');
-            para3.setAttribute('class', 'price_cart');
-            para1.appendChild(namenode);
-            para2.appendChild(amountnode);
-            para3.appendChild(pricenode);
-            para4.appendChild(spacenode); //space
-            para6.appendChild(priceendnode);
-            cart.appendChild(para1);
-            cart.appendChild(para4); //space
-            cart.appendChild(para2);
-            cart.appendChild(para4); //space
-            cart.appendChild(para3);
-            cart.appendChild(para6);
-            cart.appendChild(para5);
+            var spacenode1 = document.createTextNode(" ");
+            var spacenode2 = document.createTextNode(" ");
+            var priceendnode = document.createTextNode(":-");
+            
+            var addbtn = document.createTextNode("+");
+            var subbtn = document.createTextNode("-");
+            
+            var namepar = document.createElement("p");
+            var amntpar = document.createElement("p");
+            var pricepar = document.createElement("p");
+            var space1 = document.createElement("p");
+            var space2 = document.createElement("p");
+            var br1 = document.createElement("br");
+            var br2 = document.createElement("br");
+            var endpar = document.createElement("p");
+            
+            var btn1 = document.createElement("BUTTON");
+            var btn2 = document.createElement("BUTTON");
+            
+            namepar.setAttribute('class', 'drink_cart');
+            amntpar.setAttribute('class', 'amount_cart');
+            pricepar.setAttribute('class', 'price_cart');
+            
+            btn1.setAttribute('type', 'button');
+            btn1.setAttribute('class', 'addbtn');
+            var strArg1 = 'cartbtn("+", "' + drink_name + '")';
+            btn1.setAttribute('onclick', strArg1);
+            btn2.setAttribute('type', 'button');
+            btn2.setAttribute('class', 'subbtn');
+            var strArg2 = 'cartbtn("-", "' + drink_name + '")';
+            btn2.setAttribute('onclick', strArg2);
+            
+            namepar.appendChild(namenode);
+            amntpar.appendChild(amountnode);
+            pricepar.appendChild(pricenode);
+            space1.appendChild(spacenode1);
+            space2.appendChild(spacenode2);
+            endpar.appendChild(priceendnode);
+            
+            btn1.appendChild(addbtn);
+            btn2.appendChild(subbtn);
+            
+            cart.appendChild(namepar);
+            cart.appendChild(br1);
+            cart.appendChild(amntpar);
+            cart.appendChild(space1);
+            cart.appendChild(btn1);
+            cart.appendChild(btn2);
+            cart.appendChild(space2);
+            cart.appendChild(pricepar);
+            cart.appendChild(endpar);
+            cart.appendChild(br2);
             
         }
         var prices = cart.getElementsByClassName('price_cart');
@@ -116,6 +145,60 @@ function drop(dropevent) {
   var nodeCopy = document.getElementById(data).cloneNode(true);
   nodeCopy.id = "drink_in_cart";
   dropevent.target.appendChild(nodeCopy);*/
+}
+
+/*Increases or decreases amount, price and sum when +/- buttons in cart are pressed*/
+function cartbtn(btn, drink_name) {
+    var cart = document.getElementById('shop');
+    var inCart = cart.getElementsByClassName('drink_cart');
+    var cartAmount = cart.getElementsByClassName('amount_cart');
+    var cartPrice = cart.getElementsByClassName('price_cart');
+    var drinkList = JSON.parse(localStorage.getItem('drinkList'));
+    var drinkData = drinkList.data;
+    var nmn, price, units;
+    for(var i=0; i<20; i++) {
+        nmn = drinkData[i].namn + " " + drinkData[i].namn2;
+        if(nmn == drink_name) {
+            price = parseFloat(drinkData[i].price);
+            units = parseInt(drinkData[i].count);
+            break;
+        }
+    }
+    
+    var sum = 0;
+    for(i=0; i<cartPrice.length; i++) {
+        sum += parseFloat(cartPrice[i].innerHTML);
+    }
+    
+    for(var i=0; i<inCart.length; i++) {
+        if(inCart[i].innerHTML == drink_name) {
+            var amount;
+            
+            if(btn == "+") {
+                amount = parseInt(cartAmount[i].innerHTML) + 1;
+                sum += price;
+            } else {
+                amount = parseInt(cartAmount[i].innerHTML) - 1;
+                sum -= price;
+            }
+            
+            if(amount <= 0) {
+                var x = inCart[i].nextSibling;
+                while(x && (x.getAttribute('class') != 'drink_cart')) {
+                    x.parentNode.removeChild(x);
+                    x = inCart[i].nextSibling;
+                }
+                inCart[i].parentNode.removeChild(inCart[i]);
+            } else if(amount <= units) {
+                cartAmount[i].innerHTML = amount;
+                cartPrice[i].innerHTML = Number((amount*price).toFixed(1));
+            }
+            var sum_round = Number((sum).toFixed(1));
+            document.getElementById('totalamount').innerHTML = "Total: " + sum_round + ":-";
+            break;
+        }
+    }
+    
 }
 
 /* Clears what's currently in the cart by setting all text to nothing */
